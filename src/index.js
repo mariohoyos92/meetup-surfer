9fd1ae7cff2334ccfcecca57fbd5a704636a107f
Mario Hoyos <mariohoyos92@gmail.com>
2018-03-22 08:22:17 -0500

    refactor: add tests folder, add components folder, rearrange bootstrap files accordingly

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./ducks/store/store";
 
-import App from "./App";
+import App from "./components/App/App";
 
 import "./index.css";
 
