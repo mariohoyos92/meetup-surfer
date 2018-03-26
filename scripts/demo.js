90ac38b3ee0d95b2fb4ecd4f564eb79a270e53e7
Mario Hoyos <mariohoyos92@gmail.com>
2018-03-26 14:41:09 -0500

    feat: add demo script for mac and windows

---
+++ scripts/demo.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = "development";
+process.env.NODE_ENV = "development";
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on("unhandledRejection", err => {
+  throw err;
+});
+
+const openBrowser = require("react-dev-utils/openBrowser");
+
+openBrowser("http://localhost:3001");
